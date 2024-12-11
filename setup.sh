8b1787ef18fb11ddf9c85958c78e08c3541aada1
Francesco215 <f.sacco@protonmail.com>
2024-12-11 18:01:42 +0100

    removed useless files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-apt update
-apt install tmux -y
-pip install swig uv nvitop 
-git config --global user.email "f.sacco@protonmail.com"
-git config --global user.name "Francesco215"
-alias auv='uv venv; . .venv/bin/activate'
-uv venv 
-. .venv/bin/activate
-uv pip install -e .
